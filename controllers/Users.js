73ae3a7bb431f763ff0a4ab418670f970f73acf9
lealalves <leealalves@hotmail.com>
2022-11-22 12:18:51 -0300

    new controllers for user: addUser and listUsers

---
+++ controllers/Users.js
@@ -0,0 +1,27 @@
+
+const User = require('../models/User')
+
+const addUser = async (req, res) => {
+	
+	const username = req.body.username
+
+	const user = new User({
+		username: username
+	})
+
+	await user.save()
+	.then(data => {
+		return res.send({username: data.username, _id: data._id})
+	})
+	.catch(err => {
+		res.send(err.errors.username.properties['message'])
+	})
+}
+
+const listUsers = async (req, res) => {
+	let users = await User.find()
+
+	res.send(users)
+}
+
+module.exports = { addUser, listUsers }
