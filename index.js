6fd6235bbb50d0424d40631fb26812ceab90a7e5
lealalves <leealalves@hotmail.com>
2022-11-22 12:19:20 -0300

    added routes and config .env

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const mongoose = require('mongoose')
+
+const userController = require('./controllers/Users')
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.catch(err => console.log(err))
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.route('/api/users')
+.post((req, res) => userController.addUser(req, res))
+.get((req, res) => userController.listUsers(req, res))
 
 
 
