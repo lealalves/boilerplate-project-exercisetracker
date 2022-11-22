49885a21a4d1963413624cad3553427eaad46457
lealalves <leealalves@hotmail.com>
2022-11-22 15:40:29 -0300

    create Exercise model

---
+++ models/Exercise.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const exerciseSchema = new mongoose.Schema({
+	_id: {
+		required: true,
+		type: mongoose.Types.ObjectId,
+		ref: 'User'
+	},
+	description: {
+		required: true,
+		type: String
+	},
+	duration: {
+		required: true,
+		type: Number
+	},
+	date: {
+		type: Date,
+		default: Date.now()
+	}
+})
+
+const Exersice = mongoose.model('Exercise', exerciseSchema)
+
+module.exports = Exercise
